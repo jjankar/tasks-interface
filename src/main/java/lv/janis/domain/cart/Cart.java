0b8573ef0f6bb3fd1ea82efbf752d97e7cba36e4
Janis <jjankar@gmail.com>
2016-09-05 11:42:54 +0300

    Cart edited

--- src/main/java/lv/janis/domain/cart/Cart.java
+++ src/main/java/lv/janis/domain/cart/Cart.java
@@ -1,9 +1,11 @@
 package lv.janis.domain.cart;
 
+import java.util.Date;
 import lv.janis.domain.money.Money;
 import lv.janis.domain.money.MoneyInterface;
 import lv.janis.domain.product.ProductInterface;
 import lv.janis.domain.stock.Stock;
+import org.apache.tomcat.jni.Time;
 import org.springframework.data.annotation.Id;
 
 /**
@@ -13,7 +15,9 @@ import org.springframework.data.annotation.Id;
 public class Cart extends Stock implements CartInterface {
 
     @Id
-    String costumerData;
+    private String costumer;
+    private Date date;
+    private Time time;
     private MoneyInterface subTotal;
     private MoneyInterface vatAmount;
     private MoneyInterface total;
@@ -27,23 +31,23 @@ public class Cart extends Stock implements CartInterface {
 
     @Override
     public MoneyInterface getSubtotal() {
-        this.subTotal = new Money(0, 0);
+        this.setSubTotal(new Money(0, 0));
         for (ProductInterface product : this.array) 
         {
-            subTotal.setMajorUnit(subTotal.getMajorUnit() + product.getPrice().getMajorUnit());
-            subTotal.setMinorUnit(subTotal.getMinorUnit() + product.getPrice().getMinorUnit());
+            getSubTotal().setMajorUnit(getSubTotal().getMajorUnit() + product.getPrice().getMajorUnit());
+            getSubTotal().setMinorUnit(getSubTotal().getMinorUnit() + product.getPrice().getMinorUnit());
         }
-        int vatMinor = subTotal.getMinorUnit() % 100;
-        int vatMajor = subTotal.getMinorUnit() / 100;
-        subTotal.setMinorUnit(vatMinor);
-        subTotal.setMajorUnit(subTotal.getMajorUnit() + vatMajor);
+        int vatMinor = getSubTotal().getMinorUnit() % 100;
+        int vatMajor = getSubTotal().getMinorUnit() / 100;
+        getSubTotal().setMinorUnit(vatMinor);
+        getSubTotal().setMajorUnit(getSubTotal().getMajorUnit() + vatMajor);
 
-        return subTotal;
+        return getSubTotal();
     }
     
     @Override
     public MoneyInterface getVatAmount() {
-        this.vatAmount = new Money(0, 0);
+        this.setVatAmount(new Money(0, 0));
         for (ProductInterface product : this.array) 
         {
             int totalCents = product.getPrice().getMajorUnit() * 100 + product.getPrice().getMinorUnit();
@@ -57,12 +61,82 @@ public class Cart extends Stock implements CartInterface {
 
     @Override
     public MoneyInterface getTotal() {
-        this.total = new Money(0, 0);
-        int minor = (subTotal.getMinorUnit()+vatAmount.getMinorUnit())%100;
-        int major = (subTotal.getMinorUnit()+vatAmount.getMinorUnit())/100;
+        this.setTotal(new Money(0, 0));
+        int minor = (getSubTotal().getMinorUnit()+vatAmount.getMinorUnit())%100;
+        int major = (getSubTotal().getMinorUnit()+vatAmount.getMinorUnit())/100;
         total.setMinorUnit(minor);
         total.setMajorUnit(this.getSubtotal().getMajorUnit() + this.getVatAmount().getMajorUnit()+major);
         return total;
+    }
+
+    /**
+     * @return the costumerData
+     */
+    public String getCostumerData() {
+        return costumer;
+    }
+
+    /**
+     * @param costumerData the costumerData to set
+     */
+    public void setCostumerData(String costumer) {
+        this.costumer = costumer;
+    }
+
+    /**
+     * @return the subTotal
+     */
+    public MoneyInterface getSubTotal() {
+        return subTotal;
+    }
+
+    /**
+     * @param subTotal the subTotal to set
+     */
+    public void setSubTotal(MoneyInterface subTotal) {
+        this.subTotal = subTotal;
+    }
+
+    /**
+     * @param vatAmount the vatAmount to set
+     */
+    public void setVatAmount(MoneyInterface vatAmount) {
+        this.vatAmount = vatAmount;
+    }
+
+    /**
+     * @param total the total to set
+     */
+    public void setTotal(MoneyInterface total) {
+        this.total = total;
+    }
+
+    /**
+     * @return the date
+     */
+    public Date getDate() {
+        return date;
+    }
+
+    /**
+     * @param date the date to set
+     */
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    /**
+     * @return the time
+     */
+    public Time getTime() {
+        return time;
+    }
+
+    /**
+     * @param time the time to set
+     */
+    public void setTime(Time time) {
+        this.time = time;
     }
 
 }
